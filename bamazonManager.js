0018e54e0003be564587d50459d2be563bd89428
AndyEm <andyem64@gmail.com>
2017-04-19 17:33:06 -0400

    Challenge 2 add quantity complete

--- bamazonManager.js
+++ bamazonManager.js
@@ -64,12 +64,12 @@ function customerRequest() {
 					type:'rawlist',
 					name: 'choice',
 					message: 'What would you like to do?',
-					choices:['Add new item', 'Add qunatity to existing item']
+					choices:['View Low Inventory','Add new product', 'Add quantity to existing item']
 				}
 
 	])
 	.then(function(answer) {
-		if(answer.choice === 'Add qunatity to existing item'){
+		if(answer.choice === 'Add quantity to existing item'){
 			updateItem();
 		} 
 
@@ -80,18 +80,20 @@ function updateItem() {
 	inquirer.prompt([
 			{
 				type:'input',
-				name:'name',
+				name:'product',
 				message:'What is the name of the product'
 			}
 		]).then(function(answer){
 		var query = 'SELECT product_name FROM products';
-		var product = answer.name;
+		var product = answer.product;
 			connection.query(query,function(err,res,fields){
 				if(err) throw err;
 				//Checks if product_name is valid
 				for(var i = 0; i <res.length;i++){
-					if(res[i].product_name === answer.name){
-						console.log(res[i].product_name);
+					if(res[i].product_name === answer.product){
+						//Testing Show products name
+						console.log('Product Name' , res[i].product_name);
+						console.log('count:' , count);
 						count++;
 					}
 				}
@@ -107,17 +109,36 @@ function updateItem() {
 							message: 'How many would you like to add ?'		
 						}
 
-					]).then(function(answer){
-						var query ='SET SQL_SAFE_UPDATES = 0 UPDATE products SET ? WHERE ?'
+					]).then(function(answer) {
+						//Get stock quantity from product that was choosen
+						var query = 'SELECT * FROM products WHERE ?'; 
+						var stockQty = 0;
+						var quantity = parseInt(answer.quantity);
 						connection.query(query,[
-							{
-								stock_quantity: //quantity TRY TO GET STOCK QUANITY
-							},
 							{
 								product_name: product
 							}
-						])
-					})
+						], function(err,res,fields) {
+							stockQty = parseInt(res[0].stock_quantity);
+							// Update database 
+							console.log('Stock Qty: ' , stockQty);
+							console.log('quantity: ' , quantity);
+							console.log('Stock Qty: ' , product);
+							var query ='UPDATE products SET ? WHERE ?'
+							connection.query(query,[
+								{
+									stock_quantity: stockQty + quantity
+								},
+								{
+									product_name: product
+								}
+							],function(err,res,fields) {
+								if(err) throw err;
+								console.log("Quantity added!!!");
+								displayTable();
+							});
+						}); 
+					});
 				}
 				else{
 					console.log("That item does not exist");
